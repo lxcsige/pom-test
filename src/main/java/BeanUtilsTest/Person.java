03dc48310ab1b69beee065c84bb1e3cb6bd92d82
four <xucheng.liu@dianping.com>
2017-08-01 19:38:18 +0800

    BeanUtils

---
+++ src/main/java/BeanUtilsTest/Person.java
@@ -0,0 +1,57 @@
+package BeanUtilsTest;
+
+/**
+ * Created by lxcfour on 2017/7/31.
+ */
+public class Person {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private int height;
+
+    private int weight;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/BeanUtilsTest/PersonDTO.java
@@ -0,0 +1,56 @@
+package BeanUtilsTest;
+
+/**
+ * Created by lxcfour on 2017/7/31.
+ */
+public class PersonDTO {
+    private int id;
+
+    private String name;
+
+    private String age;
+
+    private String height;
+
+    private Object weight;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public Object getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Object weight) {
+        this.weight = weight;
+    }
+}
